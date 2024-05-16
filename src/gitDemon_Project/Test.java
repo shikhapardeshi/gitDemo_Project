3f3d3f38c080c1c681b39fdaebb8a1293ab35d7b
DELL <DELL@DESKTOP-P8PC8L5>
2024-05-16 15:36:59 -0700

    First commit...

---
+++ src/gitDemon_Project/Test.java
@@ -0,0 +1,11 @@
+package gitDemon_Project;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module gitDemon_Project {
+}
